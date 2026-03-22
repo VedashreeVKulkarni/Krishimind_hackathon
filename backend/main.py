27fc900aacc8d2e360fa782bbd1364d0519f71a6
Vedashree V Kulkarni <vedashree369@gmail.com>
2026-03-22 16:06:12 +0530

    fixed cors

--- backend/main.py
+++ backend/main.py
@@ -46,12 +46,7 @@ app = FastAPI(
 # ─── CORS ─────────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://krishimind-ten.vercel.app"
-        "https://krishimind-ten.vercel.app/",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
